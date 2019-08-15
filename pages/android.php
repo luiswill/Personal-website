993bf6558ee5ae3c3fd307eaf55272e7e1cafa63
luiswill <luis.willnat.lw@gmail.com>
2019-08-15 16:42:21 +0200

    Added android applications

---
+++ pages/android.php
@@ -0,0 +1,73 @@
+<?php
+include_once('../templates/header.php');
+?>
+<div class="blogPost">
+    <div class="container">
+        <div class="row">
+            <div class="category category--foodsharing col-12 col-sm-4">
+                <div class="overlay">
+                    <div class="textImage">Android applications</div>
+                </div>
+            </div>
+            <div class="blog__content col-12 col-sm-8">
+            During my free time on holidays or stemester breaks, I was able to develop some Android Applications you can find below : 
+
+            <ul>
+            
+
+            <li><h1><a href="./foodsharing.php">Food Sharing</h1></a><br/>
+                <li><h1><a href="https://apkpure.com/tap-bitcoin-world/com.luis.bitcoinsimulation" target="_blank">Tap Bitcoin World</h1></a><br/>
+                <p><b>App description</b><br/>
+                Play a clicker game and bring your country number 1 in the world ! * Simulation Game - No real bitcoins *
+
+                    - Invest in your country ! <br/>
+                    - Upgrade infrastructure in your home and country to earn even more<br/>
+                    - Start mining bitcoin. <br/>
+                    - Casual clicker game <br/>
+                    - Buy and sell bitcoins with real-time market's price.<br/>
+                    - Test your trading skills.<br/>
+
+                    <br/>
+                    <a href="https://drive.google.com/open?id=1OkvfZE4Owl0MhGI_DQNV-_Nub3pFRNQs">Download APK file</a>
+                    <br/>
+                    
+                    </p> 
+                    
+                    <img width="150" src="https://image.winudf.com/v2/image/Y29tLmx1aXMuYml0Y29pbnNpbXVsYXRpb25fc2NyZWVuXzBfMTUwOTYxNjA0OF8wMDQ/screen-0.jpg?fakeurl=1&type=.jpg"/>
+                    <img width="150" src="https://image.winudf.com/v2/image/Y29tLmx1aXMuYml0Y29pbnNpbXVsYXRpb25fc2NyZWVuXzNfMTUwOTYxNjA1Ml8wMzE/screen-3.jpg?fakeurl=1&type=.jpg"/>
+                    <img width="150" src="https://image.winudf.com/v2/image/Y29tLmx1aXMuYml0Y29pbnNpbXVsYXRpb25fc2NyZWVuXzRfMTUwOTYxNjA1M18wNjE/screen-4.jpg?fakeurl=1&type=.jpg"/>
+                    <img width="150" src="https://image.winudf.com/v2/image/Y29tLmx1aXMuYml0Y29pbnNpbXVsYXRpb25fc2NyZWVuXzFfMTUwOTYxNjA1MF8wMzM/screen-1.jpg?fakeurl=1&type=.jpg"/>
+                </li>
+ <br/><br/><br/>
+                <li><h1><a href="https://androidappsapk.co/detail-niice-me/" target="_blank">Niice.me</h1></a><br/>
+                <p><b>App description</b><br/>
+                - Send your friends some niice words to make them smile.<br/>
+
+                - Get reviews from your friends anonymously.<br/>
+
+                - Discover what other think about you. Send your friends niice Some words to make 'em smile.<br/>
+
+                - Get reviews from your friends anonymously.<br/>
+
+                - Discover what other think about you.<br/>
+                    <br/>
+                    
+                    </p> 
+                    
+                    <img width="150" src="https://lh3.google.com/u/0/d/1cZumSpGwzY7UHAufN9jwUXmPTxZBXsbI=w1920-h888-iv1"/>
+                    <img width="150" src="https://lh3.google.com/u/0/d/1EywDAWl4JouuiJ9zRJHovsTRlbAGCo1c=w1920-h888-iv1"/>
+                    <img width="150" src="https://lh3.google.com/u/0/d/1EywDAWl4JouuiJ9zRJHovsTRlbAGCo1c=w1920-h888-iv1"/>
+                    <img width="150" src="https://lh3.google.com/u/0/d/13Vwi9kC-Rcw6iXQHmy3TOrjYkLiD8PC-=w1920-h888-iv1"/>
+                </li>
+
+            </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once('../templates/footer.php');
+?>
+
+</html>
